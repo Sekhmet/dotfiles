711977423a18d91dfd373ef5c8c3a34a826006af
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2020-01-25 14:05:02 +0100

    feat: add setup script

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+
+OH_MY_ZSH_DIR=~/.oh-my-zsh
+BASE16_SHELL_DIR=~/.config/base16-shell
+FZF_DIR=~/.fzf
+VUNDLE_DIR=~/.vim/bundle/Vundle.vim
+
+if [[ -d "$FZF_DIR" ]]; then
+	echo "fzf is already installed"
+else
+	echo "Installing fzf"
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install --key-bindings --completion --no-update-rc
+fi
+
+if [[ -d "$OH_MY_ZSH_DIR" ]]; then
+	echo "oh-my-zsh is already installed"
+else
+	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [[ -d "$BASE16_SHELL_DIR" ]]; then
+	echo "base16-shell already installed"
+else
+	echo "Installing base16-shell"
+	git clone https://github.com/chriskempson/base16-shell.git "$BASE16_SHELL_DIR"
+fi
+
+if [[ -d "$VUNDLE_DIR" ]]; then
+	echo "Vundle is already installed"
+else
+	echo "Installing Vundle and plugins"
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	vim -c :PluginInstall
+fi
+
+. ~/.zshrc
+
