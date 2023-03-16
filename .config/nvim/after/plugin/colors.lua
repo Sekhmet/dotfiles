5be20ced80403978f60b949ee52c5f1560f9fd5f
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-03-16 20:22:51 +0100

    feat: migrate nvim to lua

---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,10 @@
+function SetColors(scheme)
+  scheme = scheme or "onenord"
+
+  vim.cmd.colorscheme(scheme)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+SetColors()
---
+++ .config/nvim/lua/sekhmet/init.lua
@@ -0,0 +1,27 @@
+require("sekhmet.packer")
+require("sekhmet.remap")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.list = true
+vim.opt.listchars = { trail = "•" }
+
+vim.opt.scrolloff = 8
+
+vim.opt.termguicolors = true
