e05891b6fd81fac96fd54ddaacec34b698a52719
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2026-01-16 11:10:33 +0100

    feat(nvim): add forgefmt formatter

--- nvim/.config/nvim/lua/config/plugins/conform.lua
+++ nvim/.config/nvim/lua/config/plugins/conform.lua
@@ -10,12 +10,20 @@ return {
 			html = { "prettierd" },
 			css = { "prettierd" },
 			json = { "prettierd" },
+			solidity = { "forgefmt" },
 		},
 		default_format_opts = {
 			lsp_format = "fallback",
 		},
 		format_on_save = {
 			timeout_ms = 500,
+		},
+		formatters = {
+			forgefmt = {
+				command = "forge",
+				args = { "fmt", "-r", "-" },
+				stdin = true,
+			},
 		},
 	},
 }
