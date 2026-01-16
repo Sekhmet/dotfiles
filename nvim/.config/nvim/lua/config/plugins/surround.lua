7669cf39a1801936329581e71ef4b96a3f0ff39b
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2026-01-16 11:13:50 +0100

    feat(nvim): add mini.surround

---
+++ nvim/.config/nvim/lua/config/plugins/surround.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-mini/mini.surround",
+	config = function()
+		require("mini.surround").setup({
+			mappings = {
+				add = "ys", -- Add surrounding in Normal and Visual modes
+				delete = "ds", -- Delete surrounding
+				replace = "cs", -- Replace surrounding
+			},
+		})
+	end,
+}
